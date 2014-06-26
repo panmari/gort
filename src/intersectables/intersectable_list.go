aad90750eaf3a6ec0bf1c863c0bb852beb46324d
panmari <smmuzi@gmail.com>
2014-06-26 16:01:09 +0200

    intersectable list does now much less memory allocation

--- src/intersectables/intersectable_list.go
+++ src/intersectables/intersectable_list.go
@@ -14,12 +14,13 @@ func (list *IntersectableList) Add(intersectable util.Intersectable) {
 }
 
 func (list *IntersectableList) Intersect(ray *util.Ray) (*util.Hitrecord, bool) {
-	closestHit := new(util.Hitrecord)
-	closestHit.T = math.MaxFloat32
+	var closestHit *util.Hitrecord
+	closestT := float32(math.MaxFloat32)
 	hadHit := false
 	for _, i := range list.intersectables {
-		if hit, doesHit := i.Intersect(ray); doesHit && hit.T < closestHit.T && hit.T > 0 {
+		if hit, doesHit := i.Intersect(ray); doesHit && hit.T < closestT && hit.T > 0 {
 			closestHit = hit
+			closestT = hit.T
 			hadHit = true
 		}
 	}
