1e654c3c1b18639d6f7aa2ca6c9f61e20a2236de
panmari <smmuzi@gmail.com>
2014-07-03 17:01:27 +0200

    Implemented instance
    including test scene

--- src/intersectables/instance.go
+++ src/intersectables/instance.go
@@ -2,16 +2,50 @@ package intersectables
 
 import (
 	"github.com/ungerik/go3d/mat4"
+	"github.com/ungerik/go3d/vec3"
 	"util"
+	"materials"
 )
 
 type Instance struct {
+	t 				mat4.T 
+	tinverse		mat4.T
+	tinverseT		mat4.T
+	material 		util.Material
+	intersectable	util.Intersectable
 }
 
+// Transform the given ray into the coordinate frame of the instance and returns the resulting intersection.
 func (i *Instance) Intersect(r *util.Ray) *util.Hitrecord {
-	return new(util.Hitrecord)
+	//transform into coordinate system of instance
+	rTransformed := util.Ray{r.Origin, r.Direction}
+	i.tinverse.MulVec3(&rTransformed.Origin, 1)
+	i.tinverse.MulVec3(&rTransformed.Direction, 0)
+	h := i.intersectable.Intersect(&rTransformed)
+	if h == nil {
+		return nil
+	}
+	
+	//transform back
+	i.t.MulVec3(&h.Position, 1)
+	i.t.MulVec3(&h.W_in, 0)
+	h.W_in.Normalize()
+	
+	//use transpose of inverse for normal
+	i.tinverseT.MulVec3(&h.Normal, 0)
+	//normalize again, bc may contain scaling
+	h.Normal.Normalize()
+	return h
 }
 
-func MakeInstance(intersectable *util.Intersectable, transform *mat4.T) {
-	//TODO
+func NewDiffuseInstance(intersectable util.Intersectable, transformation mat4.T) util.Intersectable {
+	i := new(Instance)
+	i.t = transformation
+	i.tinverse = transformation
+	i.tinverse.Invert()
+	i.tinverseT = i.tinverse
+	i.tinverseT.Transpose()
+	i.material = materials.MakeDiffuseMaterial(vec3.T{1,1,1})
+	i.intersectable = intersectable
+	return i
 }
---
+++ src/scenes/instancing_test_scene.go
@@ -0,0 +1,45 @@
+package scenes
+
+import (
+	"cameras"
+	"films"
+	"github.com/ungerik/go3d/vec3"
+	"github.com/ungerik/go3d/mat4"
+	"integrators"
+	"intersectables"
+	"intersectables/csg"
+	"samplers"
+)
+
+func NewInstancingTestScene() Scene {
+	var s Scene
+	s.Filename = "instancing_test_scene"
+	width := 1280
+	height := 720
+	eye := vec3.T{0, 0, 3}
+	lookAt := vec3.T{0, 0, 0}
+	up := vec3.T{0, 1, 0}
+	var fov float32 = 60.0
+	var aspect float32 = float32(width)/ float32(height)
+	s.Camera = cameras.MakePinholeCamera(&eye, &lookAt, &up, fov, aspect, width, height)
+	s.SPP = 1
+	s.Sampler = samplers.MakeOneSampler
+	s.Film = films.MakeBoxFilterFilm(width, height)
+
+	list := intersectables.NewIntersectableList(2)
+	sphere := csg.NewDiffuseSphere(vec3.T{0, 0, 0}, 1.0)
+	//assemble the first instance
+	trans := mat4.Ident
+	trans.SetTranslation(&vec3.T{2,0,0})
+	sphere2 := intersectables.NewDiffuseInstance(sphere, trans)
+	//assemble the second instance
+	trans2 := mat4.Ident
+	trans2.SetTranslation(&vec3.T{-2,0,0})
+	sphere3 := intersectables.NewDiffuseInstance(sphere, trans2)
+	
+	list.Add(sphere, sphere2, sphere3)
+	s.Root = list
+
+	s.Integrator = integrators.MakeDebugIntegrator(s.Root)
+	return s
+}
