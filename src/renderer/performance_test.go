5bfb06a4ef75bd55fdd038df74ab0472e0a2aef0
panmari <smmuzi@gmail.com>
2014-07-03 10:02:35 +0200

    New performance test with box scene

--- src/renderer/performance_test.go
+++ src/renderer/performance_test.go
@@ -8,11 +8,22 @@ import (
 
 func BenchmarkRenderingSimpleScene(b *testing.B) {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+	s := scenes.NewSimpleScene()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		s := scenes.MakeSimpleScene()
 		if !testing.Short() {
 			s.SPP = 64
 		}
+		StartRendering(s)
+	}
+}
+
+func BenchmarkRenderingBoxScene(b *testing.B) {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	s := scenes.NewBoxScene()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		//setting SPP doesn't change anything, has OneSampler
 		StartRendering(s)
 	}
 }
