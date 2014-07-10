8651659757b54ad495cb730e738ef149c2dab862
panmari <smmuzi@gmail.com>
2014-07-10 17:37:30 +0200

    started on uniform sampler
    ...will need changes to sampler interface

---
+++ src/samplers/uniform_sampler.go
@@ -0,0 +1,29 @@
+package samplers
+
+import (
+	"github.com/barnex/fmath"
+)
+
+type UniformSampler struct {
+	
+}
+
+/*
+* Expects n to be a square number.
+*/
+func (r *UniformSampler) Get2DSamples(n int) [][2]float32 {
+	k := fmath.Ceil(fmath.Sqrt(float32(n)))
+	k_int := int(k)
+	dist := 1/k
+	offset := 1/(k*2)
+	samples := make([][2]float32, k_int*k_int)
+	for i := range samples {
+		samples[i] = [2]float32{offset + dist*float32(i % k_int), offset + dist*float32(i / k_int)}
+	}
+	return samples
+}
+
+//TODO: seed?
+func NewUniformSampler(seed int64) Sampler {
+	return new(UniformSampler)
+}
