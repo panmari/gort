aabe75117464d895a59e4de85e3ba1093ef3888a
panmari <smmuzi@gmail.com>
2014-07-25 12:05:35 +0200

    extended tests for uniform samples

--- src/samplers/uniform_sampler_test.go
+++ src/samplers/uniform_sampler_test.go
@@ -4,6 +4,32 @@ import (
 	"testing"
 )
 
+func TestUniformSamplerOneSample(t *testing.T) {
+	s := MakeUniformSampler(0)
+	sample := s.Get2DSamples(1)
+	if sample[0] != [2]float32{0.5, 0.5} {
+		t.Error(sample)
+	}
+}
+
+func TestUniformSamplerFourSample(t *testing.T) {
+	s := MakeUniformSampler(0)
+	sample := s.Get2DSamples(4)
+	
+	if sample[0] != [2]float32{0.25, 0.25} {
+		t.Error(sample)
+	}
+	if sample[1] != [2]float32{0.75, 0.25} {
+		t.Error(sample)
+	}
+	if sample[2] != [2]float32{0.25, 0.75} {
+		t.Error(sample)
+	}
+	if sample[3] != [2]float32{0.75, 0.75} {
+		t.Error(sample)
+	}
+}
+
 func BenchmarkUniformSamplerOneSample(b *testing.B) {
 	s := MakeUniformSampler(0)
 	b.ResetTimer()
@@ -12,7 +38,7 @@ func BenchmarkUniformSamplerOneSample(b *testing.B) {
 	}
 }
 
-func BenchmarkUniformSamplerEightSamples(b *testing.B) {
+func BenchmarkUniformSamplerFourSamples(b *testing.B) {
 	s := MakeUniformSampler(0)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
