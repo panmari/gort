ea587adf60732ecf7f68115713709c4ac6cc4146
panmari <smmuzi@gmail.com>
2015-01-04 23:39:51 +0100

    Working version of faster intersect for BSP accelerator.

--- src/intersectables/accelerators/bsp_accelerator.go
+++ src/intersectables/accelerators/bsp_accelerator.go
@@ -17,7 +17,12 @@ type BSPNode struct {
 	inters      []util.Intersectable
 	box         vec3.Box
 	splitAxis   Axis
+	splitPos    float32
 	left, right *BSPNode
+}
+
+func (n *BSPNode) isLeaf() bool {
+	return n.left == nil && n.right == nil
 }
 
 func newBSPNode(box vec3.Box, axis Axis) *BSPNode {
@@ -54,11 +59,11 @@ func (acc *BSPAccelerator) buildTree(node *BSPNode, inters []util.Intersectable,
 	// Save split axis in node and prepare bounding boxes.
 	node.splitAxis = splitAxis
 	b := node.box
-	splitDist := b.Center()[splitAxis]
+	node.splitPos = b.Center()[splitAxis]
 	leftBoxMax := b.Max
-	leftBoxMax[splitAxis] = splitDist
+	leftBoxMax[splitAxis] = node.splitPos
 	rightBoxMin := b.Min
-	rightBoxMin[splitAxis] = splitDist
+	rightBoxMin[splitAxis] = node.splitPos
 
 	var leftNode, rightNode BSPNode
 	leftNode.box = vec3.Box{b.Min, leftBoxMax}
@@ -78,7 +83,71 @@ func (acc *BSPAccelerator) buildTree(node *BSPNode, inters []util.Intersectable,
 	return node
 }
 
+// A helper struct for intersect method.
+type BSPStackNode struct {
+	node       *BSPNode
+	tmin, tmax float32
+}
+
 func (acc *BSPAccelerator) Intersect(r *util.Ray) *util.Hitrecord {
+	tmin, tmax, doesIntersect := doesRayIntersectBox(r, &acc.root.box)
+	if !doesIntersect {
+		return nil
+	}
+	nodeStack := make([]*BSPStackNode, 0, 5)
+
+	var nearestHit *util.Hitrecord
+	nearestT := float32(math.MaxFloat32)
+	var node *BSPNode = acc.root
+	for node != nil {
+		if nearestT < tmin {
+			break
+		}
+		if !node.isLeaf() {
+			tSplitAxis := (node.splitPos - r.Origin[node.splitAxis]) / r.Direction[node.splitAxis]
+			var first, second *BSPNode
+
+			if r.Origin[node.splitAxis] < node.splitPos {
+				first = node.left
+				second = node.right
+			} else {
+				first = node.right
+				second = node.left
+			}
+			// process children
+			if _, _, doesIntersect := doesRayIntersectBox(r, &first.box); tSplitAxis > tmax || tSplitAxis < 0 || (math.Abs(tSplitAxis) < 1e-5 && doesIntersect) {
+				node = first
+			} else if _, _, doesIntersect := doesRayIntersectBox(r, &second.box); tSplitAxis < tmin || (math.Abs(tSplitAxis) < 1e-5 && doesIntersect) {
+				node = second
+			} else {
+				node = first
+				laterNode := BSPStackNode{second, tSplitAxis, tmax}
+				nodeStack = append(nodeStack, &laterNode)
+				tmax = tSplitAxis
+			}
+		} else {
+			for i := range node.inters {
+				hit := node.inters[i].Intersect(r)
+				if hit != nil && hit.T < nearestT && hit.T > 0 {
+					nearestT = hit.T
+					nearestHit = hit
+				}
+			}
+			if len(nodeStack) > 0 {
+				s := nodeStack[len(nodeStack)-1]
+				nodeStack = nodeStack[:len(nodeStack)-1]
+				node = s.node
+				tmin = s.tmin
+				tmax = s.tmax
+			} else {
+				break
+			}
+		}
+	}
+	return nearestHit
+}
+
+func (acc *BSPAccelerator) IntersectSlow(r *util.Ray) *util.Hitrecord {
 	nodeStack := make([]*BSPNode, 1)
 	nodeStack[0] = acc.root
 	var nearestHit *util.Hitrecord
