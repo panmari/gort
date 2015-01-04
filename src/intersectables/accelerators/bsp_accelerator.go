a8802075e944ad63871c13b0045436e36ab016e5
panmari <smmuzi@gmail.com>
2015-01-04 18:01:47 +0100

    Implemented algorithm for building BSPAccelerator.

--- src/intersectables/accelerators/bsp_accelerator.go
+++ src/intersectables/accelerators/bsp_accelerator.go
@@ -10,18 +10,23 @@ import (
 type BSPAccelerator struct {
 	n, max_depth int
 	root *BSPNode
+	box vec3.Box
 }
 
 type BSPNode struct {
-	
+	inters []util.Intersectable
+	box vec3.Box
+	splitAxis Axis
+	left, right *BSPNode
 }
 
 func newBSPNode(box vec3.Box, axis Axis) *BSPNode {
 	return new(BSPNode)
 }
 
-type Axis int
+const MIN_NR_PRIMITIVES = 5
 
+type Axis int
 const (
 	X Axis = iota
 	Y
@@ -32,10 +37,43 @@ func NewBSPAccelerator(a intersectables.Aggregate) {
 	acc := new(BSPAccelerator)
 	acc.n = a.Size()
 	acc.max_depth = int(8 + 1.3 * math.Log(float64(acc.n)) + 0.5)
-	acc.root = newBSPNode(a.GetBoundingBox(), Axis.X)
-	buildTree(acc.root, a.GetIntersectables(), 0)
+	acc.root = new(BSPNode)
+	acc.root.box = *a.BoundingBox()
+	acc.root.splitAxis = X
+	acc.buildTree(acc.root, a.GetIntersectables(), 0)
 }
 
-func buildTree(node *BSPNode, inters []util.Intersectable, depth int) *BSPNode {
-	return new(BSPNode)
+func (acc *BSPAccelerator) buildTree(node *BSPNode, inters []util.Intersectable, depth int) *BSPNode {
+	if depth > acc.max_depth || len(inters) < MIN_NR_PRIMITIVES {
+		node.inters = inters
+		return node
+	}
+	leftInters := make([]util.Intersectable, 0, len(inters)/2)
+	rightInters := make([]util.Intersectable, 0, len(inters)/2)
+	b := node.box
+	splitDist := b.Center()[node.splitAxis]
+	leftBoxMax := b.Max
+	leftBoxMax[node.splitAxis] = splitDist
+	rightBoxMin := b.Min
+	rightBoxMin[node.splitAxis] = splitDist
+	
+	var leftNode, rightNode BSPNode
+	leftNode.box = vec3.Box{b.Min, leftBoxMax}
+	rightNode.box = vec3.Box{rightBoxMin, b.Max}
+	
+	for i := range inters {
+		if leftNode.box.Intersects(inters[i].BoundingBox()) {
+			leftInters = append(leftInters, inters[i])
+		}
+		if rightNode.box.Intersects(inters[i].BoundingBox()) {
+			rightInters = append(rightInters, inters[i])
+		}
+	}
+	node.left = acc.buildTree(&leftNode, leftInters, depth + 1)
+	node.right = acc.buildTree(&rightNode, rightInters, depth + 1)
+	return node
+}
+
+func (acc *BSPAccelerator) BoundingBox() *vec3.Box {
+	return &acc.box;
 }
