d3eafba552b9e21d7342524a3d1639d15b0d9604
panmari <smmuzi@gmail.com>
2015-01-07 13:30:24 +0100

    Wrote test and extended documentation.

--- src/samplers/random_sampler_test.go
+++ src/samplers/random_sampler_test.go
@@ -19,3 +19,29 @@ func BenchmarkRandomSamplerFourSamples(b *testing.B) {
 		s.Get2DSamples(4)
 	}
 }
+
+func TestRandomSamplerTwoSamplingsAreDifferent(t *testing.T) {
+	s := NewRandomSampler(0, 4)
+	samples := s.Get2DSamples(1)
+	// samples need to be copied
+	samples_copy := make([][2]float32, len(samples))
+	copy(samples, samples_copy)
+	samples2 := s.Get2DSamples(1)
+	if len(samples) != 1 {
+		t.Errorf("Expected size 1, but was %d", len(samples))
+	}
+	if cap(samples) != 4 {
+		t.Errorf("Expected cap 4, but was %d", cap(samples))
+	}
+
+	if samples_copy[0] == samples2[0] {
+		t.Error("Samples are not different:", samples, samples2)
+	}
+	
+	// samples and samples2 should contain the same arrays, since memory
+	// was only allocated once.
+	if samples[0] != samples2[0] {
+		t.Error("Only one slice should be allocated: ", samples, samples2)
+	}
+
+}
--- src/samplers/sampler.go
+++ src/samplers/sampler.go
@@ -5,6 +5,10 @@ package samplers
 //     NewMySampler(seed int64, maxSampleCount int)
 // with seed as an initializer for random location generation and
 // maxSampleCount the maximum number n that will be used to call Get2DSamples.
+//
+// Beware, the containing arrays are only allocated once and will be reused,
+// so when calling Get2DSamples again, the values from the first time
+// will be overwritten.
 type Sampler interface {
 	Get2DSamples(n int) [][2]float32
 }
