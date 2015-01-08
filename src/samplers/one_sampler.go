9a4ef87980eeae312a1529480f86a5d7e866a1c3
panmari <smmuzi@gmail.com>
2015-01-08 17:39:12 +0100

    Minor change in OneSampler

--- src/samplers/one_sampler.go
+++ src/samplers/one_sampler.go
@@ -9,7 +9,8 @@ type OneSampler struct {
 func (o *OneSampler) Get2DSamples(n int) [][2]float32 {
 	o.samples = o.samples[0:n] // adapt size of slice
 	for i := range o.samples {
-		o.samples[i] = [2]float32{0.5, 0.5}
+		o.samples[i][0] = 0.5
+		o.samples[i][1] = 0.5
 	}
 	return o.samples
 }
