ab3f29076a3ec4e23f12b9f326418b9ad7ff2de0
panmari <smmuzi@gmail.com>
2015-01-29 09:25:14 +0100

    Removed unsignificant test.

--- src/samplers/low_discrepancy_sampler_test.go
+++ src/samplers/low_discrepancy_sampler_test.go
@@ -25,35 +25,6 @@ func BenchmarkLdSamplerFourSamples(b *testing.B) {
 	}
 }
 
-func TestModFunction(t *testing.T) {
-	r := math.Mod(5, 2)
-	if r != 1 {
-		t.Error("5 % 2 should be 1, but is ", r)
-	}
-
-	r = math.Mod(5, 0.5)
-	if r != 0 {
-		t.Error("5 % 0.5 should be 0, but is ", r)
-	}
-
-	r = math.Mod(0.5, 0.2)
-	if math.Abs(r-0.1) > EPSILON {
-		t.Error("0.5 % 0.2 should be 0.1, but is ", r)
-	}
-
-	r = math.Mod(0.2, 1)
-	if math.Abs(r-0.2) > EPSILON {
-		t.Error("0.2 % 1 should be 0.2, but is ", r)
-	}
-
-	// This is an issue!
-	r = math.Mod(2, 3)
-	if math.Abs(r-2) > EPSILON {
-		t.Error("2 % 3 should be 2, but is ", r)
-	}
-
-}
-
 func TestLdSamplerSamplings(t *testing.T) {
 	s := NewLowDiscrepancySampler(4)
 	samples := s.Get2DSamples(4)
