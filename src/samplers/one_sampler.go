80d69961976ef533c8f5c612aa42edd2c6b4e664
panmari <smmuzi@gmail.com>
2015-01-29 10:55:05 +0100

    Fixed onesampler for distributed rendering.

--- src/samplers/one_sampler.go
+++ src/samplers/one_sampler.go
@@ -3,24 +3,24 @@ package samplers
 import ()
 
 type OneSampler struct {
-	samples [][2]float32
+	Samples [][2]float32
 }
 
 func (o *OneSampler) Get2DSamples(n int) [][2]float32 {
-	o.samples = o.samples[0:n] // adapt size of slice
-	for i := range o.samples {
-		o.samples[i][0] = 0.5
-		o.samples[i][1] = 0.5
+	o.Samples = o.Samples[0:n] // adapt size of slice
+	for i := range o.Samples {
+		o.Samples[i][0] = 0.5
+		o.Samples[i][1] = 0.5
 	}
-	return o.samples
+	return o.Samples
 }
 
 func (o *OneSampler) DuplicateAndSeed(seed int64) Sampler {
-	return NewOneSampler(len(o.samples))
+	return NewOneSampler(len(o.Samples))
 }
 
 func NewOneSampler(maxSampleCount int) Sampler {
 	s := new(OneSampler)
-	s.samples = make([][2]float32, maxSampleCount)
+	s.Samples = make([][2]float32, maxSampleCount)
 	return s
 }
