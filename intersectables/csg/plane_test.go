ad6c59e3b752185ef2e2803d7801a65c7d66987d
panmari <smmuzi@gmail.com>
2019-02-16 17:27:37 +0100

    Fixing plane test.

--- intersectables/csg/plane_test.go
+++ intersectables/csg/plane_test.go
@@ -13,18 +13,18 @@ func TestPlaneIntersection(t *testing.T) {
 
 	parallelRay := util.Ray{vec3.Zero, vec3.UnitY}
 	if hit := s.Intersect(&parallelRay); hit != nil {
-		t.Errorf("Parallel ray hit plane!", hit)
+		t.Errorf("s.Intersect(parallelRay): want nil, got %v", hit)
 	}
 
 	pointingAwayRay := util.Ray{vec3.Zero, vec3.UnitX}
 	if hit := s.Intersect(&pointingAwayRay); hit != nil {
-		t.Errorf("Ray pointing away from plane hit plane!", hit)
+		t.Errorf("s.Intersect(pointingAwayRay): want nil, got %v", hit)
 	}
 
 	pointingTowardsRay := util.Ray{vec3.Zero, vec3.T{-1, 0, 0}}
 	hit := s.Intersect(&pointingTowardsRay)
 	if hit == nil {
-		t.Errorf("Ray pointing towards plane does not hit plane!", hit)
+		t.Errorf("s.Intersect(pointingTowardsRay): want not nil, got %v", hit)
 	}
 	if hit.T != 1 {
 		t.Errorf("Ray pointing towards plane hits at strange T: %f", hit.T)
