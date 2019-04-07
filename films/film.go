1eeb05241231aaf75495e9cdb93e30a5ba13a633
panmari <smmuzi@gmail.com>
2019-04-07 09:03:46 +0200

    Documenting film interface.

--- films/film.go
+++ films/film.go
@@ -5,10 +5,16 @@ import (
 	"github.com/ungerik/go3d/vec4"
 )
 
+// Film represents a datastructure for capturing samples for each pixel, then allows displaying it or exporting to png.
 type Film interface {
+	// AddSample adds a sample at position x, y. Sample is interpereted as RGB.
 	AddSample(x, y int, sample *vec3.T)
+	// WriteToPng writes the image captured by this film to the given filename.
 	WriteToPng(filename string)
+	// GetWidth returns the width of this film.
 	GetWidth() int
+	// GetHeight returns the height of this film.
 	GetHeight() int
+	// GetToneMapper returns the function used for tone mapping each pixel.
 	GetTonemapper() func(*vec4.T)
 }
